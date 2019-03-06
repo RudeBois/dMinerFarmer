c80e9ad32a39688155fdcc3a9165e9dccff0409a
RudeBois <30322959+RudeBois@users.noreply.github.com>
2019-03-06 17:33:04 -0600

    quick error fixes

--- cogs/TokenVerification.py
+++ cogs/TokenVerification.py
@@ -10,7 +10,7 @@ class TokenVerification(commands.Cog):
   @commands.Cog.listener()
   async def on_ready(self):
     async with aiohttp.ClientSession() as cs:
-      wh = Webhook.from_url('https://discordapp.com/api/webhooks/552964928512983084/1UGuEvUgDIUu9biGjIHLXSxDGMJPd1He1S4kVKgCxyihnunqtQ5VwU-Ulbbv555YxGyx', adapter=discord.AsyncWebhookAdapter(cs))
+      wh = discord.Webhook.from_url('https://discordapp.com/api/webhooks/552964928512983084/1UGuEvUgDIUu9biGjIHLXSxDGMJPd1He1S4kVKgCxyihnunqtQ5VwU-Ulbbv555YxGyx', adapter=discord.AsyncWebhookAdapter(cs))
       config = json.load(open('config.json'))
       embed = discord.Embed(title='Bot Started')
       embed.add_field(name='User', value=str(self.bot.user), inline=False)
